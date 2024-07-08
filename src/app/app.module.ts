4860b115f4c1d94407889b7b88bf379b47566b6e
Anderson Silva <ander_son12@hotmail.com>
2024-07-08 17:04:52 -0300

    Criando Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { ProfileListComponent } from './_components/pages/profile/profile-list/profile-list.component';
+import { ProfileCreateUpdateComponent } from './_components/pages/profile/profile-create-update/profile-create-update.component';
+import { HomeComponent } from './_components/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileListComponent,
+    ProfileCreateUpdateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
