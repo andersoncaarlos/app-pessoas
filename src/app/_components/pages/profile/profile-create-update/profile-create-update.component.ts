9d3e8831bba9f601f1dc93ef6e3858d7679ed818
Anderson Silva <ander_son12@hotmail.com>
2024-07-09 21:40:51 -0300

    update formReactive

--- src/app/_components/pages/profile/profile-create-update/profile-create-update.component.ts
+++ src/app/_components/pages/profile/profile-create-update/profile-create-update.component.ts
@@ -7,9 +7,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./profile-create-update.component.css']
 })
 export class ProfileCreateUpdateComponent {
+
+  onSubmit() {
+
+  }
+
   nome = '';
   profileForm = new FormGroup({
     nome: new FormControl('', Validators.required),
-    perfil: new FormControl()
+    perfil: new FormControl('', Validators.required),
+    idade: new FormControl(0, Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    pais: new FormControl(''),
+    nivelExperiencia: new FormControl('')
   })
 }
